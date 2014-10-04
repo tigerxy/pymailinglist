53a4768c1a6521e1dce0d378e3eabeadf8c6d7b7
tigerxy <roland@stempelgreim.de>
2014-10-04 13:51:22 +0200

    Create mailerconfig.py

---
+++ mailerconfig.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+host = "mail.me.com"
+user = "user"
+passwd = "pw"
+from_addr = "list@me.com"
+verteiler = ['user1@me.com','user2@me.com']
+admins = ['admin@me.com']
